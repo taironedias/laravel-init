009ebf5531f2ce6c60adf52f82c5829e93a39eeb
taironecdias <taironecdias@gmail.com>
2020-07-23 21:47:39 -0300

    ref. 06 - mod 04 : utilizando resource para o arquivo de rotas web

--- behavior/routes/web.php
+++ behavior/routes/web.php
@@ -13,15 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/**
+ * (vii)
+ */
+Route::resourceVerbs([
+    'create' => 'cadastro',
+    'edit' => 'editar'
+]);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
 
-Route::view('/form', 'form');
+// Route::view('/form', 'form');
 
 /**
  * Definição de Rota
@@ -69,36 +78,83 @@ Route::view('/form', 'form');
 /**
  * GET
  */
-Route::get('/users/1', 'UserController@index');
-Route::get('/getData', 'UserController@getData');
+// Route::get('/users/1', 'UserController@index');
+// Route::get('/getData', 'UserController@getData');
 
 /**
  * POST
  */
-Route::post('postData', 'UserController@postData');
+// Route::post('postData', 'UserController@postData');
 
 /**
  * PUT
  */
-Route::put('/users/1', 'UserController@putData');
+// Route::put('/users/1', 'UserController@putData');
 
 /**
  * PATCH
  */
-Route::patch('/users/1', 'UserController@patchData');
+// Route::patch('/users/1', 'UserController@patchData');
 
 
 /**
  * MATCH USING PUT/PATCH
  */
-Route::match(['put', 'patch'], '/users/2', 'UserController@matchData');
+// Route::match(['put', 'patch'], '/users/2', 'UserController@matchData');
 
 /**
  * DELETE
  */
-Route::delete('/users/1', 'UserController@destroy');
+// Route::delete('/users/1', 'UserController@destroy');
 
 /**
  * ANY
  */
-Route::any('/users', 'UserController@any');
+// Route::any('/users', 'UserController@any');
+
+
+/**
+ * Route::resource('BASE_URI', 'Controller');
+ * 
+ * Nesse caso, utilizando o resource não é necessário criar manualmente cada rota. Se combinado com a instrução 'php artisan make:controller NameController --resource', o Laravel já cria o todos os métodos necessários e faz o bind para cada rota, como na instrução (i)
+ * 
+ * 
+ * Se desejar criar uma rota que não seja criada automaticamente pelo resource, é interessane adicionar a rota antes da instrução Route::resource, como na instrução (ii)
+ * 
+ * Se desejar sobrescrever alguma rota que já existe no resource, deve ser adicionada depois da instrução Route::resource, como na instrução (iii)
+ * 
+ * 
+ * Se desejar ter rotas específicas para que o resource implemente, basta utilizar o método only passando um array com as rotas desejadas, como na instrução (iv)
+ * 
+ * 
+ * Se desejar ter todas as rotas implementadas pelo resource e não querer apenas uma ou algumas, basta utilizar o método except passando um array com as rotas a serem desconsideradas, como na instrução (v)
+ * 
+ * Se desejar alterar o nome padrão da rota, por exemplo edit para editar ou create para cadastro, basta no início desse arquivo de rotas, utilizar a função resourceVerbs e a cada chave existente, sobrescrever pela desejada, como na instrução (vi)
+ * Obs.1: É importante que todas os outros recursos obtenham o mesmo padrão de rotas para facilitar a manutenibilidade, por esse motivo que é recomendado adicionar essa instrução no início desse arquivo. E para API, não é recomendado esse uso!
+ * Obs.2: É alterado apenas o nome da rota, o nome das funções permanece a default.
+ */
+
+/**
+ * (ii)
+ */
+Route::get('/posts/premium', 'PostController@premium');
+
+/**
+ * (i)
+ */
+Route::resource('posts', 'PostController');
+
+/**
+ * (iv)
+ */
+// Route::resource('posts', 'PostController')->only(['index', 'show']);
+
+/**
+ * (v)
+ */
+// Route::resource('posts', 'PostController')->except(['index', 'show']);
+
+/**
+ * (iii)
+ */
+// Route::get('/posts', 'PostController@indexSobrescrito');

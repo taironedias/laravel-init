65d9a74acf6bf3308e84f8fdf041e4b618bd812b
taironecdias <taironecdias@gmail.com>
2020-07-23 21:48:08 -0300

    ref. 06 - mod 04 : utilizando resource para o arquivo de rotas api e simulando a chamada no Insomnia

--- behavior/routes/api.php
+++ behavior/routes/api.php
@@ -17,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Utilizando o resource com API. O sintaxe e regras são bem semelhantes. A diferença está na chamada do método, agora apiResource, e na criação do controller, agora php artisan make:controller API\\NameController --api
+ * Assim, é criado exatamente os mesmo métodos do resource, só que dentro da web temos duas rotas que são criadas para exibir o formulário (create e update), como aqui dentro não é necessário essas rotas, logo não faz a criação
+ * 
+ * Route::apiResource('BASE_URI', 'API\Controller');
+ * 
+ */
+Route::apiResource('users', 'API\UserController');

381697d9548095cc0e34bfee61a54b1c1bfce580
Tairone Dias <taironecdias@gmail.com>
2020-08-18 19:20:17 -0300

    06.02 parametrização do projeto

---
+++ behavior_eloquent/database/factories/PostFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->sentence(8),
+        'subtitle' => $faker->sentence(16),
+        'description' => $faker->paragraph(10)
+    ];
+});
